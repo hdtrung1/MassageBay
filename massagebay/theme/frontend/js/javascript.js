59a5460f5b5a25f3ed62fc158cdb6c949817e050
TrungHD <TrungHD@TRUNGHD>
2023-11-02 10:12:13 +0700

    style responsive mobile

---
+++ massagebay/theme/frontend/js/javascript.js
@@ -0,0 +1,8 @@
+function menuMobileFunction() {
+    var x = document.getElementById("link");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
