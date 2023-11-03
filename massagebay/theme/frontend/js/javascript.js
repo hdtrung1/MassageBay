e5398403ecfdf84b6268082272cd7d3e0c45c1f1
TrungHD <TrungHD@TRUNGHD>
2023-11-03 14:41:18 +0700

    massagebay-code

--- massagebay/theme/frontend/js/javascript.js
+++ massagebay/theme/frontend/js/javascript.js
@@ -1,3 +1,19 @@
+const zaloIcon = document.getElementById('zalo-img');
+const zaloIconHeight = zaloIcon.clientHeight;
+
+const likeBtns = document.querySelectorAll('.like-btn');
+likeBtns.forEach((btn) => {
+    btn.style.height = `${zaloIconHeight}px`;
+});
+
+const shareBtns = document.querySelectorAll('.share-btn');
+shareBtns.forEach((btn) => {
+    btn.style.height = `${zaloIconHeight}px`;
+});
+
+const messageIcon = document.querySelector('.message-icon');
+messageIcon.style.height = `${zaloIconHeight}px`;
+
 function menuMobileFunction() {
     var x = document.getElementById("link");
     if (x.style.display === "block") {
